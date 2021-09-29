254fdc8e3ac1d17bd54d00c5d497a65d2835639f
Sávio Da Costa Santos <saviocosta.flam20@gmail.com>
2021-09-29 16:45:10 -0300

    Alterações nas Telas

---
+++ app/src/main/java/com/example/testegit/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.example.testegit
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.testegit.databinding.FragmentMainBinding
+
+class MainFragment: androidx.fragment.app.Fragment(R.layout.fragment_main){
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+
+    }
+}
